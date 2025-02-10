b25223f04e2ecb87d5bd8b6db64832cec4d275eb
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-02-10 09:46:42 +0530

    set up Express server; configure middleware for CORS, logging, and body parsing, and establish MongoDB connection

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors({
+    origin : [process.env.FRONTEND_URL],
+    credentials : true
+}))
+
+
+app.use(morgan("dev"));
+app.use(express.json({ limit : "10mb" }));
+app.use(express.urlencoded({ extended : true }));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+
+
+export { app };
