bcbae6b52d639fd1a825a25012397f131b178712
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-03-18 21:57:11 +0530

    feat: add createUserAccount async action with toast notifications for account registration

--- client/src/Redux/Slices/authSlice.js
+++ client/src/Redux/Slices/authSlice.js
@@ -1,5 +1,5 @@
 
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axiosInstance from "../../helpers/axiosInstance.js";
 
 const initialState = {
@@ -7,6 +7,24 @@ const initialState = {
     userRole : localStorage.getItem("userRole") !== undefined ? localStorage.getItem("userRole") : "",
     userData : JSON.parse(localStorage.getItem("userData")) !== undefined ? JSON.parse(localStorage.getItem("userData")) : {},
 }
+
+const toastHandler = (promise, loadingMsg, successMsg, errorMsg) => {
+    return toast.promise(promise, {
+        loading: loadingMsg,
+        success: (data) => data?.data?.message || successMsg,
+        error: errorMsg,
+    });
+};
+
+export const createUserAccount = createAsyncThunk("/auth/register", async (data) => {
+    try {
+        const res = axiosInstance.post("users/register", data);
+        toastHandler(res, "Creating your account..", "Account created successfully!", "Failed to create a new account!");
+        return (await res).data;
+    } catch (err) {
+        console.error(`Error occurred in creating new account: ${err}`);
+    }
+});
 
 const authSlice = createSlice({
     name : "auth",
