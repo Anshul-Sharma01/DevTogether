671f97da78f4dd77e9c46238e4a72a0deb449b2a
Chirag5017 <chirag.bhatia.14567@gmail.com>
2025-04-14 23:49:59 +0530

    fix: bind server to all network interfaces for accessibility

--- IDE/server/src/index.js
+++ IDE/server/src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 dotenv.config({ path : "./.env" })
 const PORT = process.env.PORT || 9000
 
-server.listen(PORT, () => {
+server.listen(PORT,"0.0.0.0",  () => {
     console.log(`Server is running on PORT : ${PORT}`)
 })
 
