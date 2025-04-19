0f4e1df0fcb7372a6e5bf7ce21b0cdde7b48bb4c
Chirag5017 <chirag.bhatia.14567@gmail.com>
2025-04-19 15:05:29 +0530

    Initialize server package with Express and Nodemon dependencies

--- IDE/server/src/controllers/generateFileTree.controller.js
+++ IDE/server/src/controllers/generateFileTree.controller.js
@@ -7,19 +7,20 @@ const generateFileTree = async (directory) => {
   
     async function fileTree(directory, currentTree) {
   
-      const files = await fs.readdir(directory);
-      //console.log(files);
+      const dirents = await fs.readdir(directory , {withFileTypes : true});
+      const files =  dirents.filter(dirent => !dirent.name.startsWith('.'));
+      // console.log(files);
       for (const file of files) {
-        const filePath = path.join(directory, file);
+        const filePath = path.join(directory, file.name);
         const stat = await fs.stat(filePath);
         ///console.log(filePath);
         
   
         if (stat.isDirectory()) {
-          currentTree[file] = {};
-          await fileTree(filePath, currentTree[file]);
+          currentTree[file.name] = {};
+          await fileTree(filePath, currentTree[file.name]);
         } else {
-          currentTree[file] = null;
+          currentTree[file.name] = null;
         }
       }
     }
